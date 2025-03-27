91e00f5afda4238dba4cca34cc01bae894de2f7c
Ivanka Haidai <ivankia01@ukr.net>
2025-03-27 22:35:27 +0200

    add tasks 1-3 to module 4

--- js/task-1.js
+++ js/task-1.js
@@ -1,9 +1,25 @@
-function slugify(title) {
-  const words = title.toLowerCase().split(" ");
-  return words.join("-");
+function isEnoughCapacity(products, containerSize){
+let sum = 0;
+let productsArray = Object.values(products);
+for (const values of productsArray) {
+sum += values
+}
+if (sum > containerSize) {
+return false} else {return true}
 }
 
-console.log(slugify("Arrays for beginners")); 
-console.log(slugify("English for developer")); 
-console.log(slugify("Ten secrets of JavaScript")); 
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS"));
+console.log(
+  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+); // false
+
+console.log(
+  isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+); // false
