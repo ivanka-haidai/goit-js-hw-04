164fdb7298c2c070e5efe5cfc43eb8af13f59dbb
ivanka-haidai <ivankia01@ukr.net>
2025-03-27 22:24:00 +0200

    Initial commit

---
+++ js/task-1.js
@@ -0,0 +1,9 @@
+function slugify(title) {
+  const words = title.toLowerCase().split(" ");
+  return words.join("-");
+}
+
+console.log(slugify("Arrays for beginners")); 
+console.log(slugify("English for developer")); 
+console.log(slugify("Ten secrets of JavaScript")); 
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS"));
